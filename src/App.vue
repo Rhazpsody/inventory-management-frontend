376743710ea5cc91b4d61532d7f67b15a2efcb9b
Rhazpsody <ranggaplk09@gmail.com>
2024-10-25 12:29:10 +0700

    commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <Navbar @navigate-to="handleNavigation" />
+    <div class="main-content">
+      <div v-if="currentPage === 'user'">
+        <UserComponent />
+      </div>
+      <div v-else-if="currentPage === 'item'">
+        <ItemComponent/>
+      </div>
+      <div v-else-if="currentPage === 'transaction'">
+        <TransactionComponent/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Navbar from './components/Navbar.vue'
+  import UserComponent from './components/UserList.vue'
+  import ItemComponent from './components/ItemList.vue'
+  import TransactionComponent from './components/Transaction.vue' ;
+
+  export default {
+    components: {
+      Navbar,
+      UserComponent,
+      ItemComponent,
+      TransactionComponent 
+    },
+    data() {
+      return {
+        currentPage: 'user'
+      }
+    },
+    methods: {
+      handleNavigation(page) {
+        this.currentPage =  page;
+      }
+    }
+
+  }
+</script>
+
+<style scoped>
+#app{
+  font-family: Arial, Helvetica, sans-serif;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
+.main-content {
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px;
+}
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+       <button v-on:click="increment">Pencet Aku Sayang</button>
+       <button v-on:click="decrement">Mines</button>
+       <p> {{ counter }} </p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            counter: 0
+
+        };
+    },
+    methods: {
+        increment() {
+            this.counter++;
+        },
+        decrement() {
+            this.counter--;
+        }
+    }
+}
+</script>
